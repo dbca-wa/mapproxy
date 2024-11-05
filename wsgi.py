231adbbb10a94b94347e20faac7500faa55b596b
Ashley Felton <ashley@ropable.com>
2024-11-05 11:06:40 +0800

    Configure WSGI application logging.

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,14 @@
-# WSGI module for use with uWSGI or gunicorn
+import logging
+
 from mapproxy.wsgiapp import make_wsgi_app
 
+# Configure logging.
+logging.config.fileConfig("config/log.ini")
+
+# Set the logging level for all azure-* libraries (the azure-storage-blob library uses this one).
+# Reference: https://learn.microsoft.com/en-us/azure/developer/python/sdk/azure-sdk-logging
+azure_logger = logging.getLogger("azure")
+azure_logger.setLevel(logging.WARNING)
+
+# WSGI module for use with uWSGI or gunicorn
 application = make_wsgi_app("config/mapproxy.yaml")
